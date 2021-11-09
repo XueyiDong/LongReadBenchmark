417731fd6e9b00339a68e3ff284058fbd4bf5f48
Xueyi Dong <dong.x@wehi.edu.au>
2021-11-09 14:43:42 +1100

    add DTU exploratory plot

--- longvsshort/DTUmixres.R
+++ longvsshort/DTUmixres.R
@@ -102,3 +102,89 @@ upset(fromList(append(DTU.gene.sequin.illumina.100vs000, DTU.gene.sequin.ONT.100
       text.scale = c(1.5, 1.5, 1.5, 1.2, 1.2, 1.5),
       sets.bar.color = c("#ECD98B", "#AAAAC2",  "#03875C", "#9A4C43", "#4E3227")[c(2, 1, 5, 4, 3, 2, 5, 1, 4, 3)])
 dev.off()
+
+# some further exploration
+DTU.tx.human.ONT.100vs000 <- lapply(DTU.tx.human.ONT, function(x){
+  return(x[[1]])
+})
+DTU.tx.sequin.ONT.100vs000 <- lapply(DTU.tx.sequin.ONT, function(x){
+  return(x[[1]])
+})
+DTU.tx.human.illumina.100vs000 <- lapply(DTU.tx.human.illumina, function(x){
+  return(x[[1]])
+})
+DTU.tx.sequin.illumina.100vs000 <- lapply(DTU.tx.sequin.illumina, function(x){
+  return(x[[1]])
+})
+
+
+DTU.ONT <- Reduce(union, append(DTU.tx.human.ONT.100vs000, DTU.tx.sequin.ONT.100vs000))
+DTU.Illumina <- Reduce(union, append(DTU.tx.human.illumina.100vs000, DTU.tx.sequin.illumina.100vs000))
+DTU.ONTonly <- DTU.ONT[!(DTU.ONT %in% DTU.Illumina)]
+DTU.Illuminaonly <- DTU.Illumina[!(DTU.Illumina %in% DTU.ONT)]
+DTU.ONTandIllumina <- intersect(DTU.Illumina, DTU.ONT)
+library(edgeR)
+s <- catchSalmon(file.path("../ONT/salmon_bs", list.files("../ONT/salmon_bs")))
+dge <- DGEList(counts=s$counts/s$annotation$Overdispersion, genes=s$annotation)
+s.short <- catchSalmon(file.path("../illumina/salmon_bs", list.files("../illumina/salmon_bs")))
+dge.short <- DGEList(counts = s.short$counts/s.short$annotation$Overdispersion, genes = s.short$annotation)
+rownames(dge) <- strsplit2(rownames(dge), "|", fixed = TRUE)[,1]
+dge$genes$category <- "not DTU"
+dge$genes$category[match(DTU.ONTonly, rownames(dge))] <- "ONT only"
+dge$genes$category[match(DTU.Illuminaonly, rownames(dge))] <- "Illumina only"
+dge$genes$category[match(DTU.ONTandIllumina, rownames(dge))] <- "ONT and Illumina"
+rownames(dge.short) <- strsplit2(rownames(dge.short), "|", fixed = TRUE)[,1]
+dge.short$genes$category <- "not DTU"
+dge.short$genes$category[match(DTU.ONTonly, rownames(dge.short))] <- "ONT only"
+dge.short$genes$category[match(DTU.Illuminaonly, rownames(dge.short))] <- "Illumina only"
+dge.short$genes$category[match(DTU.ONTandIllumina, rownames(dge.short))] <- "ONT and Illumina"
+dge$samples$group <- rep(c("000", "100", "075", "050", "025"), rep(3, 5))
+dge <- dge[filterByExpr(dge),]
+dge.short$samples$group <- rep(c("000", "100", "075", "050", "025"), rep(3, 5))
+dge.short <- dge.short[filterByExpr(dge.short),]
+dge$genes$totalCount <- rowSums(dge$counts)
+dge.short$genes$totalCount <- rowSums(dge.short$counts)
+dge$genes$category <- factor(dge$genes$category, levels = c("Illumina only", "ONT and Illumina", "ONT only", "not DTU"))
+dge.short$genes$category <- factor(dge.short$genes$category, levels = c("Illumina only", "ONT and Illumina", "ONT only", "not DTU"))
+pdf("plots/DTUcategory.pdf", height = 5, width = 8)
+ggplot(dge$genes, aes(x=category, y=Length, fill=category)) +
+  geom_violin() +
+  scale_y_continuous(trans = "log10") +
+  theme_bw() +
+  theme(text = element_text(size = 16)) +
+  scale_fill_manual(values = c("#FCB344", "#A09F78", "#438DAC", "gray80"))
+ggplot(dge$genes, aes(x=category, y=EffectiveLength, fill=category)) +
+  geom_violin() +
+  scale_y_continuous(trans = "log10") +
+  theme_bw() +
+  theme(text = element_text(size = 16)) +
+  scale_fill_manual(values = c("#FCB344", "#A09F78", "#438DAC", "gray80"))
+ggplot(dge$genes, aes(x=category, y=totalCount, fill=category)) +
+  geom_violin() +
+  scale_y_continuous(trans = "log10") +
+  theme_bw() +
+  theme(text = element_text(size = 16)) +
+  scale_fill_manual(values = c("#FCB344", "#A09F78", "#438DAC", "gray80"))+
+  ggtitle("long read expression")
+ggplot(dge.short$genes, aes(x=category, y=totalCount, fill=category)) +
+  geom_violin() +
+  scale_y_continuous(trans = "log10") +
+  theme_bw() +
+  theme(text = element_text(size = 16)) +
+  scale_fill_manual(values = c("#FCB344", "#A09F78", "#438DAC", "gray80"))+
+  ggtitle("short read expression")
+ggplot(dge$genes, aes(x=category, y=Overdispersion, fill=category)) +
+  geom_violin() +
+  scale_y_continuous(trans = "log10") +
+  theme_bw() +
+  theme(text = element_text(size = 16)) +
+  scale_fill_manual(values = c("#FCB344", "#A09F78", "#438DAC", "gray80"))+
+  ggtitle("long read overdispersion")
+ggplot(dge.short$genes, aes(x=category, y=Overdispersion, fill=category)) +
+  geom_violin() +
+  scale_y_continuous(trans = "log10") +
+  theme_bw() +
+  theme(text = element_text(size = 16)) +
+  scale_fill_manual(values = c("#FCB344", "#A09F78", "#438DAC", "gray80"))+
+  ggtitle("short read overdispersion")
+dev.off()
