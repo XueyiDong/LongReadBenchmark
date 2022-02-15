1e4257e4c3644d98acd3f4afcf6e71f07d51e843
Xueyi Dong <dong.x@wehi.edu.au>
2022-02-15 16:07:06 +1100

    update length dist plot: filter out undemultiplexed

--- ONT/QC/lengthDistribution.R
+++ ONT/QC/lengthDistribution.R
@@ -7,16 +7,6 @@ qcdata$Read_length <- as.numeric(qcdata$Read_length)
 qcdata$Qscore <- as.numeric(qcdata$Qscore)
 
 # qcdata.sub <- qcdata[1:1000, ]
-pdf("plots/LengthDist.pdf", height = 5, width = 8)
-ggplot(qcdata, aes(x=Read_length)) +
-  geom_density(colour="#black") +
-  geom_histogram(aes(y=..density..), fill="#438DAC", colour="438DAC", alpha = .3, bins=50) +
-  theme_bw() +
-  theme(text = element_text(size = 20)) +
-  scale_x_continuous(trans="log10") +
-  labs(x= "ONT read length")
-dev.off()
-
 pdf("plots/LengthDistSample.pdf", height = 5, width = 8)
 qcdata$Barcode[!(qcdata$Barcode %in% c(paste0("barcode0", 1:6)))] <- "other"
 ggplot(qcdata, aes(x=Read_length, colour=Barcode, linetype=Barcode)) +
@@ -27,4 +17,15 @@ ggplot(qcdata, aes(x=Read_length, colour=Barcode, linetype=Barcode)) +
   labs(x= "ONT read length", colour="sample", linetype="sample") +
   scale_colour_manual(values = c(met.brewer("Troy", 6), "grey70")) +
   scale_linetype_manual(values = c(rep("solid", 6), "dashed"))
+dev.off()
+
+# undemultiplexed reads are filtered out
+pdf("plots/LengthDist.pdf", height = 5, width = 8)
+ggplot(qcdata[qcdata$Barcode != "other", ], aes(x=Read_length)) +
+  geom_density(colour="black") +
+  geom_histogram(aes(y=..density..), fill="#438DAC", colour="#438DAC", alpha = .3, bins=50) +
+  theme_bw() +
+  theme(text = element_text(size = 20)) +
+  scale_x_continuous(trans="log10") +
+  labs(x= "ONT read length")
 dev.off()
