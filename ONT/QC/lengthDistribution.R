3a13eb37042e29f07e28db4cee636c1bd1ac85cb
Xueyi Dong <dong.x@wehi.edu.au>
2022-02-15 15:39:29 +1100

    ONT length distribution plot

---
+++ ONT/QC/lengthDistribution.R
@@ -0,0 +1,30 @@
+library(ggplot2)
+library(MetBrewer)
+
+qcdata <- readRDS("/stornext/General/data/user_managed/grpu_mritchie_1/XueyiDong/long_read_benchmark/ONT/QC/summaryInfo.RDS")
+qcdata <- as.data.frame(qcdata, stringsAsFactors = FALSE)
+qcdata$Read_length <- as.numeric(qcdata$Read_length)
+qcdata$Qscore <- as.numeric(qcdata$Qscore)
+
+# qcdata.sub <- qcdata[1:1000, ]
+pdf("plots/LengthDist.pdf", height = 5, width = 8)
+ggplot(qcdata, aes(x=Read_length)) +
+  geom_density(colour="#438DAC") +
+  geom_histogram(aes(y=..density..), fill="#438DAC", colour="black", alpha = .5) +
+  theme_bw() +
+  theme(text = element_text(size = 20)) +
+  scale_x_continuous(trans="log10") +
+  labs(x= "ONT read length")
+dev.off()
+
+pdf("plots/LengthDistSample.pdf", height = 5, width = 8)
+qcdata$Barcode[!(qcdata$Barcode %in% c(paste0("barcode0", 1:6)))] <- "other"
+ggplot(qcdata, aes(x=Read_length, colour=Barcode, linetype=Barcode)) +
+  geom_density() +
+  theme_bw() +
+  theme(text = element_text(size = 20)) +
+  scale_x_continuous(trans="log10") +
+  labs(x= "ONT read length", colour="sample", linetype="sample") +
+  scale_colour_manual(values = c(met.brewer("Troy", 6), "grey70")) +
+  scale_linetype_manual(values = c(rep("solid", 6), "dashed"))
+dev.off()
