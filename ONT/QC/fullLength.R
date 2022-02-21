af68a0b582ec241bd9053c0899d694768f8a3f54
Xueyi Dong <dong.x@wehi.edu.au>
2022-02-21 16:31:37 +1100

    update full length prop script

--- ONT/QC/fullLength.R
+++ ONT/QC/fullLength.R
@@ -6,53 +6,93 @@ library(ggplot2)
 library(data.table)
 library(GenomicFeatures)
 library(Hmisc)
-# read and calculate, save to RDS
-dir_bam  <- "/stornext/General/data/user_managed/grpu_mritchie_1/XueyiDong/long_read_benchmark/ONT/transcriptome_mapping/split"
-bams <- list.files(path = dir_bam, pattern=".bam$")
-args = commandArgs(trailingOnly=TRUE)
-i = as.numeric(args[1])
-# for (i in 1:6){
-  bam1 <- suppressWarnings(readBam(file.path(dir_bam, bams[i])))
-  readDF <- makeReadDf(bam1)
-  summ <- makeSummaryList(bam1)
-  saveRDS(readDF, file = paste("./full_length/", bams[i], ".readDF.RDS"))
-  saveRDS(summ, file= paste("./full_length/", bams[i], ".summ.RDS"))
-  saveRDS(bam1, file= paste("./full_length/", bams[i], ".bam.RDS"))
-  rm(bam1)
-  gc()
+library(edgeR)
+
+#-----------
+# # read and calculate, save to RDS
+# dir_bam  <- "/stornext/General/data/user_managed/grpu_mritchie_1/XueyiDong/long_read_benchmark/ONT/transcriptome_mapping/split"
+# bams <- list.files(path = dir_bam, pattern=".bam$")
+# args = commandArgs(trailingOnly=TRUE)
+# i = as.numeric(args[1])
+#   bam1 <- suppressWarnings(readBam(file.path(dir_bam, bams[i])))
+#   readDF <- makeReadDf(bam1)
+#   summ <- makeSummaryList(bam1)
+#   saveRDS(readDF, file = paste0("./full_length/test/", bams[i], ".readDF.RDS"))
+#   saveRDS(summ, file= paste0("./full_length/test/", bams[i], ".summ.RDS"))
+#   saveRDS(bam1, file= paste0("./full_length/test/", bams[i], ".bam.RDS"))
+#   rm(bam1)
+#   gc()
+
+# #--------------
+# # combine readDF for each sample
+# dir_rds <- "/stornext/General/data/user_managed/grpu_mritchie_1/XueyiDong/long_read_benchmark/ONT/QC/full_length"
+# args = commandArgs(trailingOnly=TRUE)
+# bc = as.numeric(args[1])
+# files = list.files(path = dir_rds, pattern="readDF.RDS$")
+# smpl = grep(paste0("^barcode0", bc), files)
+# rdf = data.frame()
+# for(i in smpl){
+#   cat(files[i])
+#   cat("\n")
+#   tmp = readRDS(file.path(dir_rds, files[i]))
+#   rdf = rbind(rdf, tmp)
 # }
+# saveRDS(rdf, file.path(dir_rds, "readDF", paste0("barcode0", bc, ".readDF.RDS")))
+
+#---------------
+# # combine all RDF
+# dir_rdf <- "/stornext/General/data/user_managed/grpu_mritchie_1/XueyiDong/long_read_benchmark/ONT/QC/full_length/readDF"
+# files = list.files(path = dir_rdf, pattern="readDF.RDS$")
+# readDF = data.frame()
+# for(i in 1:6){
+#     tmp = readRDS(file.path(dir_rdf, files[i]))
+#     readDF = rbind(readDF, tmp)
+#     rm(tmp)
+#     gc()
+# }
+# saveRDS(rdf, "readDF.RDS")
 
 #------------------------
-# 
-# # prepare for plotting
-# 
-# # attach tx len from annotation to readDF
-# anno <- read.delim("/stornext/General/data/user_managed/grpu_mritchie_1/XueyiDong/annotation/rnasequin_isoforms_2.4.tsv", sep = "\t", stringsAsFactors = FALSE)
-# readDF$tx_len <- anno$LENGTH[match(readDF$seqnames, anno$NAME)]
-# # remove 'novel' isoforms
+# prepare for plotting
+cat("reading RDF.", "\n")
+readDF <- readRDS("readDF.RDS")
+cat("RDF loaded.", "\n")
+readDF$seqnames <- as.character(readDF$seqnames)
+readDF$rname <- as.character(readDF$rname)
+# get tx len and attach to readDF
+cat("reading DGE", "\n")
+dge <- readRDS("../../longvsshort/dge.rds")
+readDF$tx_len <- dge$genes$Length[match(readDF$seqnames, rownames(dge))]
+# remove 'novel' isoforms
 # readDF$novelty <- is.na(readDF$tx_len)
 # readDF <- readDF[!readDF$novelty,]
-# maxLength = max(readDF$tx_len)
-# readDF$txLengthGroup <- cut2(readDF$tx_len, cuts = c(0, 500, 1000, 1500,
-#                                              2000, maxLength))
-# readDF$covFraction <- readDF$width / readDF$tx_len
-# library(viridis)
-# #calculate some stats by transcript
-# txStat <- sapply(unique(readDF$seqnames), function(x){
-#   readDF.sel = readDF[readDF$seqnames==x, ]
-#   meanCovFrac = mean(readDF.sel$covFraction)
-#   medianCovFraction = median(readDF.sel$covFraction)
-#   fl95 = sum(readDF.sel$covFraction >= 0.95) / nrow(readDF.sel)
-#   fl90 = sum(readDF.sel$covFraction >= 0.90) / nrow(readDF.sel)
-#   c(readDF.sel[1, "tx_len"], meanCovFrac, medianCovFraction, fl95, fl90, nrow(readDF.sel))
-# }, simplify = TRUE)
-# # Fig 3C
-# pdf("plots/txLenFL.pdf", height = 5, width = 8)
-# ggplot(txStat, aes(x=tx_len, y=fl95, colour = log_count))+
-#   scale_x_continuous(trans = "log10") +
-#   geom_point() +
-#   labs(x = "Annotated transcript length", y = "Fraction of full-length", colour = "log count") +
-#   theme_bw() +
-#   theme(text = element_text(size = 20)) +
-#   scale_colour_viridis()
-# dev.off()
+maxLength = max(readDF$tx_len)
+readDF$txLengthGroup <- cut2(readDF$tx_len, cuts = c(0, 500, 1000, 1500,
+                                             2000, maxLength))
+readDF$covFraction <- readDF$width / readDF$tx_len
+library(viridis)
+#calculate some stats by transcript
+cat("calculating tx stats.", "\n")
+txStat <- sapply(unique(readDF$seqnames), function(x){
+  readDF.sel = readDF[readDF$seqnames==x, ]
+  meanCovFrac = mean(readDF.sel$covFraction)
+  medianCovFrac = median(readDF.sel$covFraction)
+  fl95 = sum(readDF.sel$covFraction >= 0.95) / nrow(readDF.sel)
+  fl90 = sum(readDF.sel$covFraction >= 0.90) / nrow(readDF.sel)
+  c(readDF.sel[1, "tx_len"], meanCovFrac, medianCovFrac, fl95, fl90, nrow(readDF.sel))
+}, simplify = TRUE)
+txStat <- as.data.frame(t(txStat))
+colnames(txStat) <- c("tx_len", "mean", "median", "fl95", "fl90", "count")
+txStat$log_count <- log(txStat$count)
+saveRDS(txStat, "txStat.RDS")
+cat("making plot.", "\n")
+# Fig 3C
+pdf("plots/txLenFL.pdf", height = 5, width = 8)
+ggplot(txStat, aes(x=tx_len, y=fl95, colour = log_count))+
+  scale_x_continuous(trans = "log10") +
+  geom_point() +
+  labs(x = "Annotated transcript length", y = "Fraction of full-length", colour = "log count") +
+  theme_bw() +
+  theme(text = element_text(size = 20)) +
+  scale_colour_viridis()
+dev.off()
