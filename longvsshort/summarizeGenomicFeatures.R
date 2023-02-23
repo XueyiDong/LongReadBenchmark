82f5a2172064b62267e4b2a32c994061bdba5d88
Xueyi Dong <dong.x@wehi.edu.au>
2023-02-23 18:01:44 +1100

    update genomic feature count stat plot

--- longvsshort/summarizeGenomicFeatures.R
+++ longvsshort/summarizeGenomicFeatures.R
@@ -1,12 +1,12 @@
 gene.counts.long <- readRDS("counts/gene.counts.long.RDS")
 exon.counts.long <- readRDS("counts/exon.counts.long.RDS")
 intron.counts.long <- readRDS("counts/intron.counts.long.RDS")
-intergenic.counts.long <- readRDS("counts/intergenic.counts.long.RDS")
+# intergenic.counts.long <- readRDS("counts/intergenic.counts.long.RDS")
 
 gene.counts.short <- readRDS("counts/gene.counts.short.RDS")
 exon.counts.short <- readRDS("counts/exon.counts.short.RDS")
 intron.counts.short <- readRDS("counts/intron.counts.short.RDS")
-intergenic.counts.short <- readRDS("counts/intergenic.counts.short.RDS")
+# intergenic.counts.short <- readRDS("counts/intergenic.counts.short.RDS")
 
 #Check that the file order is the same between all the objects
 
@@ -47,23 +47,31 @@ samples <- data.frame(
 samples.long <- samples.short <- samples
 
 # Calculate count number
-samples.long$Total.Reads <- colSums(exon.counts.long$stat[,-1])
+samples.long$Total.Reads <- colSums(exon.counts.long$stat[,-1]) - unlist(exon.counts.long$stat[10, -1])
 samples.long$Unmapped.reads <- unlist(exon.counts.long$stat[2, -1])
 samples.long$Mapped <- samples.long$Total.Reads - samples.long$Unmapped.reads
 
-samples.long$subread.gene <- colSums(gene.counts.long$counts)
-samples.long$subread.exon <- colSums(exon.counts.long$counts)
-samples.long$subread.intron <- colSums(intron.counts.long$counts)
-samples.long$subread.intergenic <- colSums(intergenic.counts.long$counts)
+# samples.long$subread.gene <- colSums(gene.counts.long$counts)
+# samples.long$subread.exon <- colSums(exon.counts.long$counts)
+# samples.long$subread.intron <- colSums(intron.counts.long$counts)
+# samples.long$subread.intergenic <- colSums(intergenic.counts.long$counts)
+samples.long$subread.gene <- unlist(gene.counts.long$stat[1, -1])
+samples.long$subread.exon <- unlist(exon.counts.long$stat[1, -1])
+samples.long$subread.intron <- samples.long$subread.gene - samples.long$subread.exon
+samples.long$subread.intergenic <- samples.long$Mapped - samples.long$subread.gene
 
 samples.short$Total.Reads <- colSums(exon.counts.short$stat[,-1])
 samples.short$Unmapped.reads <- unlist(exon.counts.short$stat[2, -1])
 samples.short$Mapped <- samples.short$Total.Reads - samples.short$Unmapped.reads
 
-samples.short$subread.gene <- colSums(gene.counts.short$counts)
-samples.short$subread.exon <- colSums(exon.counts.short$counts)
-samples.short$subread.intron <- colSums(intron.counts.short$counts)
-samples.short$subread.intergenic <- colSums(intergenic.counts.short$counts)
+# samples.short$subread.gene <- colSums(gene.counts.short$counts)
+# samples.short$subread.exon <- colSums(exon.counts.short$counts)
+# samples.short$subread.intron <- colSums(intron.counts.short$counts)
+# samples.short$subread.intergenic <- colSums(intergenic.counts.short$counts)
+samples.short$subread.gene <- unlist(gene.counts.short$stat[1, -1])
+samples.short$subread.exon <- unlist(exon.counts.short$stat[1, -1])
+samples.short$subread.intron <- samples.short$subread.gene - samples.short$subread.exon
+samples.short$subread.intergenic <- samples.short$Mapped - samples.short$subread.gene
 
 samples.long <- data.table::melt(samples.long, id.vars = c("group", "sample"),
                                  variable.name = "Category")
@@ -75,7 +83,19 @@ samples <- rbind(samples.long, samples.short)
 
 write.csv(samples, file = "counts/features.mapping.summary.csv")
 
+library(dplyr)
+samples4plot <- samples %>% 
+  filter(Category %in% c("Unmapped.reads", "subread.exon", "subread.intron", "subread.intergenic"))
+
 library(ggplot2)
-ggplot(samples, aes(x=sample, y=value, fill=Category)) +
-  geom_bar(stat = "identity") +
-  facet_grid(cols = vars(Dataset))
+pdf("plots/featuredCountStat.pdf", height = 5, width = 8)
+ggplot(samples4plot, aes(x=sample, y=value, fill=Category)) +
+  geom_bar(stat = "identity", position = "fill") +
+  facet_grid(cols = vars(Dataset)) +
+  labs(y = "Proportion", x = "Sample") +
+  theme_bw() +
+  scale_fill_brewer(palette = "Dark2", 
+                    labels = c("Unmapped reads", "Exonic reads", "Intronic reads", "Intergenic reads")) +
+  theme(text = element_text(size = 16),
+        axis.text.x = element_text(angle = 30, hjust = 1))
+dev.off()
